b55702755dde09a13e1c5af2d4579f0c7d4be7aa
Your Name <you@example.com>
2023-01-03 20:06:42 -0300

    adding service worker separated from mainactivity

--- SPEEDMEASURES/app/src/main/java/com/gladic/speedmeasures/MainActivity.java
+++ SPEEDMEASURES/app/src/main/java/com/gladic/speedmeasures/MainActivity.java
@@ -1,19 +1,15 @@
 package com.gladic.speedmeasures;
 
-import android.Manifest;
 import android.content.ClipData;
 import android.content.ClipboardManager;
+import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.content.SharedPreferences;
-import android.content.pm.PackageManager;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
-import android.os.Build;
-import android.support.annotation.RequiresApi;
-import android.support.v4.app.ActivityCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
@@ -23,13 +19,30 @@ import android.widget.Toast;
 
 import java.text.DecimalFormat;
 
-public  class MainActivity extends AppCompatActivity implements LocationListener {
-    protected LocationManager locationManager;
+public  class MainActivity extends AppCompatActivity {
 
+    private servicespeedmeasures mService ;
+    private boolean mBound = false;
+
+    /** Defines callbacks for service binding, passed to bindService() */
+    private ServiceConnection connection = new ServiceConnection() {
+
+        @Override
+        public void onServiceConnected(ComponentName className, IBinder service) {
+            // We've bound to LocalService, cast the IBinder and get LocalService instance
+            mService = ((servicespeedmeasures.LocalBinder)service).getService();
+            mService.settextView(tspeed,speedconvention,distancemade);
+            mService.setdistancedone(distancedone);
+            mBound = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName arg0) {
+            mBound = false;
+        }
+    };
     private static final String PREFS_NAME = "com.gladic.speedmeasures";
     private static final String PREFS_DISTANCE_STRING = "distancestring";
-
-    private String startlongitude,startlatitude,endlongitude,endlatitude;
 
     private String measureunit ="";
 
@@ -99,32 +112,19 @@ public  class MainActivity extends AppCompatActivity implements LocationListener
             distancemade.setText(String.valueOf((speedconvert == 0)?distancedone:distancedone * 0.6213712f) + measureunit);
         }
 
-
-        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
-
         conversormeasure.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
                 if(speedconvert == 0)
                 {
-                    speedconvert = 1;
+                    mService.setspeedconversor(1);
+
                     speedconvention.setText("MI/H");
                     conversormeasure.setText("MI/H");
 
                 }else {
-                    speedconvert = 0;
+                    mService.setspeedconversor(0);
                     speedconvention.setText("KM/H");
                     conversormeasure.setText("KM/H");
                 }
@@ -138,10 +138,10 @@ public  class MainActivity extends AppCompatActivity implements LocationListener
             public void onClick(View view) {
 
                 ClipboardManager clipboard = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
-                ClipData clip = ClipData.newPlainText("latlong", endlatitude + " " + endlongitude);
+                ClipData clip = ClipData.newPlainText("latlong", mService.getlatitude() + " " + mService.getlongitude());
                 clipboard.setPrimaryClip(clip);
 
-                Toast.makeText(ctx, endlatitude + " " + endlongitude , Toast.LENGTH_LONG).show();
+                Toast.makeText(ctx, mService.getlatitude() + " " + mService.getlongitude() , Toast.LENGTH_LONG).show();
             }
         });
 
@@ -150,7 +150,7 @@ public  class MainActivity extends AppCompatActivity implements LocationListener
             public void onClick(View view) {
 
                 ClipboardManager clipboard = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
-                ClipData clip = ClipData.newPlainText("google", "https://maps.google.com/maps?q=" + endlatitude + ',' + endlongitude);
+                ClipData clip = ClipData.newPlainText("google", "https://maps.google.com/maps?q=" + mService.getlatitude() + ',' + mService.getlongitude());
                 clipboard.setPrimaryClip(clip);
 
                 Toast.makeText(ctx, "Done" , Toast.LENGTH_LONG).show();
@@ -162,7 +162,7 @@ public  class MainActivity extends AppCompatActivity implements LocationListener
             public void onClick(View view) {
 
                 ClipboardManager clipboard = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
-                ClipData clip = ClipData.newPlainText("waze", "https://waze.com/ul?ll=" + endlatitude + ',' + endlongitude);
+                ClipData clip = ClipData.newPlainText("waze", "https://waze.com/ul?ll=" + mService.getlatitude() + ',' + mService.getlongitude());
                 clipboard.setPrimaryClip(clip);
 
                 Toast.makeText(ctx, "Done" , Toast.LENGTH_LONG).show();
@@ -173,78 +173,47 @@ public  class MainActivity extends AppCompatActivity implements LocationListener
             @Override
             public void onClick(View view) {
 
-                distancedone = 0;
-                distancemade.setText(0 + "");
+                  distancedone = 0;
+                //  distancemade.setText(0 + "");
+                if (mBound) {
 
-                Toast.makeText(ctx, "Done" , Toast.LENGTH_LONG).show();
+                    mService.cleantotaldistance();
+
+                    Toast.makeText(ctx, "Done", Toast.LENGTH_LONG).show();
+                }
             }
         });
     }
 
     @Override
-    public void onLocationChanged(Location location) {
+    protected void onStart() {
+        super.onStart();
+        Intent intent = new Intent(this, servicespeedmeasures.class);
+        this.bindService(intent, connection, Context.BIND_AUTO_CREATE);
+        startService(intent);
+    }
 
-        if(location.isFromMockProvider())
-        {
-            return;
-        }
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
 
-        int speed =0;
-
-        startlatitude = endlatitude;
-        startlongitude = endlongitude;
-
-        endlatitude = String.valueOf(location.getLatitude());
-        endlongitude = String.valueOf(location.getLongitude());
-
-        switch(speedconvert) {
-            case 0 :
-                speed = (int) ((location.getSpeed() * 3600) / 1000);
-                break;
-            case 1:
-                speed = (int) (location.getSpeed() * 2.2369);
-                break;
-        }
-
-        String get_final_velocity = (speed > 9) ? "0" + String.valueOf(speed) : (speed > 99) ? String.valueOf(speed) : "00" + String.valueOf(speed);
-        tspeed.setText(get_final_velocity);
-
-        if(endlatitude == null || startlatitude == null)
-        {}
-        else {
-            location.distanceBetween(Float.valueOf(startlatitude), Float.valueOf(startlongitude), Float.valueOf(endlatitude), Float.valueOf(endlongitude), results);
-
-            if (results.length > 0 && speed > 0) {
-                distancedone = distancedone + results[0];
-
-                if(distancedone > 1000)
-                {
-                    if(speedconvert == 0)
-                        measureunit = "km";
-                    else
-                        measureunit = "mi";
-
-                    distancemade.setText(df.format((speedconvert == 0)?distancedone / 1000 :distancedone * 0.6213712f) + measureunit);
-                }else
-                {
-                    if(speedconvert == 0)
-                        measureunit = "m";
-                    else
-                        measureunit = "mi";
-
-                    distancemade.setText(String.valueOf((speedconvert == 0)?distancedone:distancedone * 0.6213712f) + measureunit);
-                }
-
-            }
-        }
-
-
+    @Override
+    protected void onDestroy()
+    {
+        super.onDestroy();
+        unbindService(connection);
+        mBound = false;
+        stopService(new Intent(this,servicespeedmeasures.class));
     }
 
     @Override
     public void onPause()
     {
         final SharedPreferences.Editor edit = mPrefs.edit();
+
+        distancedone = mService.getdistancedone();
+        //Log.d("bound", "distance" + distancedone);
 
         if(speedconvert == 0)
         {}
@@ -257,20 +226,5 @@ public  class MainActivity extends AppCompatActivity implements LocationListener
         edit.commit();
 
         super.onPause();
-    }
-
-    @Override
-    public void onProviderDisabled(String provider) {
-        //Log.d("Latitude","disable");
-    }
-
-    @Override
-    public void onProviderEnabled(String provider) {
-      //  Log.d("Latitude","enable");
-    }
-
-    @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {
-       // Log.d("Latitude","status");
     }
 }
---
+++ SPEEDMEASURES/app/src/main/java/com/gladic/speedmeasures/servicespeedmeasures.java
@@ -0,0 +1,216 @@
+package com.gladic.speedmeasures;
+
+import static java.lang.System.exit;
+
+import android.Manifest;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.AsyncTask;
+import android.os.Binder;
+import android.os.IBinder;
+import android.support.v4.app.ActivityCompat;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.text.DecimalFormat;
+
+/*
+* https://developer.android.com/guide/components/services.html
+* https://developer.android.com/guide/components/bound-services.html
+* https://stackoverflow.com/questions/39507506/how-to-execute-background-task-when-android-app-is-closed-set-to-background
+* */
+
+public class servicespeedmeasures extends Service implements LocationListener {
+
+    private IBinder binder;
+
+    boolean allowRebind; // indicates whether onRebind should be used
+
+    protected LocationManager locationManager;
+
+    private String startlongitude,startlatitude,endlongitude,endlatitude;
+
+    private String measureunit ="";
+
+    private TextView tspeed;
+    private TextView speedconvention;
+    private TextView distancemade;
+
+    private int speedconvert = 0;
+
+    private float[] results = {0, 0, 0, 0};;
+    private float distancedone=0;
+
+    DecimalFormat df = new DecimalFormat();
+
+
+    /**
+     * Class used for the client Binder.  Because we know this service always
+     * runs in the same process as its clients, we don't need to deal with IPC.
+     */
+
+
+    @Override
+    public void onCreate() {
+
+        binder = new LocalBinder();
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            //return -1;
+            exit(-1);
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+    }
+
+    public class LocalBinder extends Binder {
+        servicespeedmeasures getService() {
+            // Return this instance of LocalService so clients can call public methods
+            return servicespeedmeasures.this;
+        }
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // A client is binding to the service with bindService()
+        Log.d("bound", "bound");
+        return binder;
+    }
+    @Override
+    public boolean onUnbind(Intent intent) {
+        // All clients have unbound with unbindService()
+        return allowRebind;
+    }
+    @Override
+    public void onRebind(Intent intent) {
+        // A client is binding to the service with bindService(),
+        // after onUnbind() has already been called
+    }
+    @Override
+    public void onDestroy() {
+        // The service is no longer used and is being destroyed
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        // The service is starting, due to a call to startService()
+        /*AsyncTask.execute(new Runnable() {
+            @Override
+            public void run() {
+                while(true){}
+            }
+        });
+        */
+        return android.app.Service.START_STICKY;
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+        if(location.isFromMockProvider())
+        {
+            return;
+        }
+
+        int speed =0;
+
+        startlatitude = endlatitude;
+        startlongitude = endlongitude;
+
+        endlatitude = String.valueOf(location.getLatitude());
+        endlongitude = String.valueOf(location.getLongitude());
+
+        switch(speedconvert) {
+            case 0 :
+                speed = (int) ((location.getSpeed() * 3600) / 1000);
+                break;
+            case 1:
+                speed = (int) (location.getSpeed() * 2.2369);
+                break;
+        }
+
+        String get_final_velocity = (speed > 9) ? "0" + String.valueOf(speed) : (speed > 99) ? String.valueOf(speed) : "00" + String.valueOf(speed);
+        tspeed.setText(get_final_velocity);
+
+        if(endlatitude == null || startlatitude == null)
+        {}
+        else {
+            location.distanceBetween(Float.valueOf(startlatitude), Float.valueOf(startlongitude), Float.valueOf(endlatitude), Float.valueOf(endlongitude), results);
+
+            if (results.length > 0 && speed > 0) {
+                distancedone = distancedone + results[0];
+
+                if(distancedone > 1000)
+                {
+                    if(speedconvert == 0)
+                        measureunit = "km";
+                    else
+                        measureunit = "mi";
+
+                    distancemade.setText(df.format((speedconvert == 0)?distancedone / 1000 :distancedone * 0.6213712f) + measureunit);
+                }else
+                {
+                    if(speedconvert == 0)
+                        measureunit = "m";
+                    else
+                        measureunit = "mi";
+
+                    distancemade.setText(String.valueOf((speedconvert == 0)?distancedone:distancedone * 0.6213712f) + measureunit);
+                }
+
+            }
+        }
+    }
+
+    public void settextView(TextView tspeed,TextView speedconvention,TextView distancemade)
+    {
+        this.tspeed = tspeed;
+        this.speedconvention = speedconvention;
+        this.distancemade = distancemade;
+    }
+
+    public String getlatitude()
+    {
+        return endlatitude;
+    }
+
+    public String getlongitude()
+    {
+        return endlongitude;
+    }
+
+    public float getdistancedone()
+    {
+        return distancedone;
+    }
+
+    public void setdistancedone(float value)
+    {
+        distancedone = value;
+    }
+
+    public void cleantotaldistance()
+    {
+        distancedone = 0;
+        distancemade.setText(0 + "");
+    }
+
+    public void setspeedconversor(int value)
+    {
+        speedconvert = value;
+    }
+
+}
